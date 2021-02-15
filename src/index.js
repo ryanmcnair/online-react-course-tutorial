656e82c02b7c5cd7cafafe20468b77932ae6ef08
Ryan McNair <ryan.mcnair@gmail.com>
2021-02-15 11:51:20 -0600

    finished up to video 24

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+//CSS
 import './index.css'
-
+// Setup variables
+const author = 'Grogu'
+const title = 'The child';
+const img = 'https://47caza48tvn318lyrs17w45k-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/baby-yoda-300x300.jpg'
 function BookList() {
   return (
     <section className='book-list'>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      <Book job='developer'/>
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return <article className='book'>
-    <Image />
-    <Title />
-    <Author />
+    <img
+      src={img}
+      alt={title}
+    />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>
 };
-
-const Image = () => (
-  <img
-    src='https://47caza48tvn318lyrs17w45k-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/baby-yoda-300x300.jpg'
-    alt=''
-  />
-);
-
-const Title = () => <h1>The Child</h1>;
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>Baby Yoda</h4>;
-
 
 ReactDom.render(<BookList />, document.getElementById('root'));
